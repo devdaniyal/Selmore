6bfbb5db16186e66ab1c5191b1c45c60aa8796af
DevFarzan <farzanhanif123@gmail.com>
2019-04-18 17:03:45 +0500

    changing in all

--- router.js
+++ router.js
@@ -9,5 +9,6 @@ module.exports = function(app){
   //post routes
 //app.post('/signin',requireSignin,  Authentication.signin);
 app.post('/signup',Authentication.signup);
+app.post('/signin',requireSignin, Authentication.signin);
   //app.get('/getprofile',requireAuth, getprofile.getProfile)
 }
