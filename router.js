59d8798df003c343c78830c97602f46d980d3ba6
DevFarzan <farzanhanif123@gmail.com>
2019-04-18 12:17:51 +0500

    changing in all

---
+++ router.js
@@ -0,0 +1,13 @@
+const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+const requireAuth = passport.authenticate('jwt', { session:false });
+const requireSignin = passport.authenticate('local', {session:false});
+
+module.exports = function(app){
+
+  //post routes
+//app.post('/signin',requireSignin,  Authentication.signin);
+app.post('/signup',Authentication.signup);
+  //app.get('/getprofile',requireAuth, getprofile.getProfile)
+}
